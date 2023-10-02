8730897c55c42ac9e93ac08bd884b5bd3e27be12
someone <someone.email.com>
2023-10-02 11:28:29 +0100

    some of today's tasks

---
+++ coding_fundamentals/task_01/task_03.py
@@ -0,0 +1,5 @@
+length = int(input("Input rectangle length: "))
+width = int(input("Input rectangle width: "))
+area = length * width
+
+print(f"Your rectangle of {length} x {width} had and area of {area}.")
