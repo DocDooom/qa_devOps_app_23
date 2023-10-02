053c849f60e503021cbd2d21b11735b2676cd49c
someone <someone.email.com>
2023-10-02 14:11:09 +0100

    updating selection_part1

--- coding_fundamentals/task_02/selection_part1.py
+++ coding_fundamentals/task_02/selection_part1.py
@@ -1,8 +1,8 @@
 user_age = int(input("How old are you: "))
 
-if user_age > 18:
+if user_age >= 18:
     print("You are in cat A")
-elif user_age > 16:
+elif user_age >= 16:
     print("You are in cat B")
 else:
     print("You are in cat C")
