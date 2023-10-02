5a8da06fbd660ffa8d1385b56ab893be670cd2f0
someone <someone.email.com>
2023-10-02 11:36:15 +0100

    updates

--- coding_fundamentals/task_01/task_03.py
+++ coding_fundamentals/task_01/task_03.py
@@ -1,5 +1,8 @@
 length = int(input("Input rectangle length: "))
 width = int(input("Input rectangle width: "))
 area = length * width
+perimeter = 2 * (length + area)
 
-print(f"Your rectangle of {length} x {width} had and area of {area}.")
+
+print(f"Your rectangle of {length} x {width} has an area of {area}.")
+print(f"Your rectangle of {length} x {width} has a perimeter of {perimeter}.")
