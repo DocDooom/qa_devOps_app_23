6349762c630e4a5d1bfb368347e7fd0cc4dfa6cb
DocDooom <kidsonictrading@gmail.com>
2023-10-04 14:13:44 +0100

    bug fixes

--- coding_fundamentals/task_07/task_07_01_v2.py
+++ coding_fundamentals/task_07/task_07_01_v2.py
@@ -19,13 +19,11 @@ with open(file_path, mode='r') as file:
         monthly_data = next(csv_reader)  # Read the next row for monthly data
         
         # Convert strings to integers
-        monthly_data = [int(i) for i in monthly_data[0].split(',')]
-        print(company_name)
-        print(monthly_data)
+        monthly_data = [int(i) for i in monthly_data]
 
         # Add up the monthly data to monthly_sums
         monthly_sums = [sum(x) for x in zip(monthly_sums, monthly_data)]
-
+        
         total_man.append((company_name, sum(monthly_data)))
 
 # Print the results
@@ -35,6 +33,7 @@ print(*monthly_sums, sep="\n")
 print("Total: ", sum(monthly_sums))
 
 print("====================================================")
+
 
 for company, total in total_man:
     print(company)
