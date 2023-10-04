622b30a2751c66759ba7a0282abccaae25489405
DocDooom <kidsonictrading@gmail.com>
2023-10-04 11:43:52 +0100

    Working tax calc but totally inaccurategit add *!

--- coding_fundamentals/task_06/tax.py
+++ coding_fundamentals/task_06/tax.py
@@ -2,13 +2,17 @@ def get_income_tax(salary: float) -> float:
     p_allow = 11850
     taxable = salary - p_allow
     print("Taxable Income: ", taxable)
+    tax = 0
 
     if salary in range(0, 34501):
-        return taxable * 0.2
+        return round(taxable * 0.2, 2)
     elif salary in range(34501, 150000):
-        return taxable * 0.4
+        tax +=(taxable - 34501) * 0.4
+        tax += taxable * 0.2
+        return round(tax, 2)
     else:
-        return taxable * 0.45
+        tax += (taxable - 150000) * 0.45
+        tax += (taxable - 34501) * 0.4
 
 
 print(get_income_tax(float(input("Enter Salary: "))))
